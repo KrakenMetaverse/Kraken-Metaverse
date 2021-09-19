9db2b231f9631b0ebf71d94d3bf244d52545f083
KrakenMetaverse <krakenmetaverse.2021@gmail.com>
2021-09-19 18:51:32 +0100

    First commit

---
+++ buy.js
@@ -0,0 +1,35 @@
+// Set Date
+var countDownDate = new Date("oct 1, 2021 21:00:00").getTime();
+
+// CountDown Action
+var x = setInterval(function() {
+    var now           = new Date().getTime();
+    var distance      = countDownDate - now;
+    var days          = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours         = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes       = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds       = Math.floor((distance % (1000 * 60)) / 1000);
+    var output        = "", minutesResult, hoursResult, daysResult;
+
+    if (days >= 0) {
+        daysResult = days;
+    }
+    if (hours >= 0) {
+        hoursResult = hours;
+    }
+    if (minutes >= 0) {
+        minutesResult = minutes;
+    }
+    
+    document.getElementsByClassName("days")[0].innerHTML = daysResult;
+    document.getElementsByClassName("hours")[0].innerHTML = hoursResult;
+    document.getElementsByClassName("minutes")[0].innerHTML = minutesResult;
+    document.getElementsByClassName("seconds")[0].innerHTML = seconds;
+    
+    if (distance < 0) {
+        clearInterval(x);
+
+
+        document.getElementsByClassName('finalText')[0].innerHTML = "It's time to Buy";
+    }
+}, 100);    
